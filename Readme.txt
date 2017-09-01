3d8c5eaf0fdbd8f5ad351d8b6298624be435f528
jash-git <jash.liao@gmail.com>
2017-09-01 12:01:06 +0800

    Create
    https://github.com/jash-git/SQL-LEFT-JOIN-Multi-table.git

---
+++ Readme.txt
@@ -0,0 +1,59 @@
+﻿SQL LEFT JOIN 多表
+
+
+資料來源:http://blog.csdn.net/dxnn520/article/details/8281467
+
+
+表1：------------------------------ 【人事表】
+     表名：【man】
+     欄位： Man_Id,Man_Name，Zw_Id
+              1     貂蟬       2
+              2     曹操       1
+
+表2：------------------------------ 【職務表】
+     表名：【zw】
+     欄位： Zw_Id,Zw_Name
+              1     校長 
+              2     班主任
+
+表3：------------------------------ 【工資表】
+     表名：【gZ】
+     欄位： Man_Id,Money，Sex_Id
+              1    2500     1
+              2    4000     2
+
+表3：------------------------------ 【性別表】
+     表名：【xb】
+     欄位： Sex_Id,Sex_Name
+              1    女
+              2    男
+
+需要的------------------------------- 【查詢結果】：
+
+     Man_Id,Man_Name,sex_name,Zw_Name,Money
+       1      貂蟬     女      班主任   2500
+       2      曹操     男       校長    4000
+	   
+
+ANS01[推薦]:
+	SELECT a.man_id,man_name,d.sex_name,zw_name,c.money  
+	FROM man AS a   
+      LEFT JOIN zw AS b ON a.zw_id=b.zw_id   
+      LEFT JOIN gz AS c ON a.man_id=c.man_id   
+      LEFT JOIN xb AS d ON c.sex_id=d.sex_id 
+
+ANS02:
+	SELECT Man_Id,Man_Name,sex_name,Zw_Name,Money FROM  
+	(  
+		   SELECT  a.Man_Id,Man_Name,b.Zw_Id,Zw_Name,Money,Sex_Id  FROM  [man] a   
+				   LEFT JOIN  [zw] b ON a.Zw_Id=b.Zw_Id  
+				   LEFT JOIN  [gz] c ON a.Man_Id=c.Man_Id  
+	)t  
+	LEFT JOIN xb m ON m.Sex_Id=t.Sex_Id
+
+ANS03:
+	SELECT man.man_id,man.man_name,xb.sex_name,zw_name,gz.money  
+	FROM man   
+      LEFT JOIN zw ON man.zw_id=zw.zw_id   
+      LEFT JOIN gz ON man.man_id=gz.man_id   
+      LEFT JOIN xb ON gz.sex_id=xb.sex_id     	
---
+++ SQL Files/test_left_join[有缺資料情況].sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.2
+-- http://www.phpmyadmin.net
+--
+-- 主機: localhost
+-- 產生日期: 2017 年 09 月 01 日 03:54
+-- 伺服器版本: 5.1.44-community
+-- PHP 版本: 5.4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 資料庫: `test_left_join`
+--
+CREATE DATABASE IF NOT EXISTS `test_left_join` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `test_left_join`;
+
+-- --------------------------------------------------------
+
+--
+-- 表的結構 `gz`
+--
+
+CREATE TABLE IF NOT EXISTS `gz` (
+  `Man_Id` int(11) NOT NULL,
+  `Money` int(11) NOT NULL,
+  `Sex_Id` int(11) NOT NULL,
+  PRIMARY KEY (`Man_Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 轉存資料表中的資料 `gz`
+--
+
+INSERT INTO `gz` (`Man_Id`, `Money`, `Sex_Id`) VALUES
+(1, 2500, 1),
+(2, 4000, 2);
+
+-- --------------------------------------------------------
+
+--
+-- 表的結構 `man`
+--
+
+CREATE TABLE IF NOT EXISTS `man` (
+  `Man_Id` int(11) NOT NULL,
+  `Man_Name` text NOT NULL,
+  `Zw_Id` int(11) NOT NULL,
+  PRIMARY KEY (`Man_Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 轉存資料表中的資料 `man`
+--
+
+INSERT INTO `man` (`Man_Id`, `Man_Name`, `Zw_Id`) VALUES
+(1, '貂蟬', 2),
+(2, '曹操', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 表的結構 `xb`
+--
+
+CREATE TABLE IF NOT EXISTS `xb` (
+  `Sex_Id` int(11) NOT NULL,
+  `Sex_Name` text NOT NULL,
+  PRIMARY KEY (`Sex_Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 轉存資料表中的資料 `xb`
+--
+
+INSERT INTO `xb` (`Sex_Id`, `Sex_Name`) VALUES
+(1, '女');
+
+-- --------------------------------------------------------
+
+--
+-- 表的結構 `zw`
+--
+
+CREATE TABLE IF NOT EXISTS `zw` (
+  `Zw_Id` int(11) NOT NULL,
+  `Zw_Name` text NOT NULL,
+  PRIMARY KEY (`Zw_Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 轉存資料表中的資料 `zw`
+--
+
+INSERT INTO `zw` (`Zw_Id`, `Zw_Name`) VALUES
+(1, '校長');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
